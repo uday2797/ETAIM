d0f80ac193ce8bbb449ce09714d9e7e6d38f2f0c
uday2797 <54591473+uday2797@users.noreply.github.com>
2025-10-28 10:33:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ const styles = `
     
     #map {
         width: 100%;
-        height: 45px; /* ↓ reduced height */
+        height: 60px; /* ↓ reduced height */
         border-radius: 10px;
         border: 1px solid #ddd;
         overflow: hidden;
